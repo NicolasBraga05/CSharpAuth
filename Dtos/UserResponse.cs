dc7f62a34511db07176c634324ecb90911ba3791
Nicolas <devnicolas.work@gmail.com>
2025-08-29 00:29:34 -0300

    feat: add User Dto

---
+++ Dtos/UserResponse.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSharpAuth.Models;
+
+namespace CSharpAuth.Dtos
+{
+    public class UserResponse
+    {
+        public string Message { get; set; }
+
+        public UserDto User { get; set; }
+    }
+
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
